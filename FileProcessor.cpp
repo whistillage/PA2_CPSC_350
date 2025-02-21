5dc758d91ce491ac0fc76354678bb7ddc9dcd3df
greatttay <kthdream02@gmail.com>
2025-02-21 14:47:45 -0800

    edit comments in 'FileProcessor.cpp'

--- FileProcessor.cpp
+++ FileProcessor.cpp
@@ -9,19 +9,20 @@ FileProcessor::FileProcessor() {
 FileProcessor::~FileProcessor() {
 }
 
+/* read Input File and save the game information in _gameinfo[8]*/
+/*
+Line # 1 is the number of levels, L (a positive integer)
+Line # 2 is the dimension of the grid, N (a positive integer)
+Line # 3 is the number of initial lives, V (a positive integer)
+Line # 4 is the approximate percentage of the positions in each level with coins (a positive integer)
+Line # 5 is the approximate percentage of the positions in each level with nothing (a positive integer)
+Line # 6 is the approximate percentage of the positions in each level with Goombas (a positive integer)
+Line # 7 is the approximate percentage of the positions in each level with Koopas (a positive integer)
+Line # 8 is the approximate percentage of the positions in each level with mushrooms (a positive integer)
+
+Values on line 4-8 MUST sum to 100.
+*/
 void FileProcessor::readInputFile(const char* inputFile){
-    /*
-    Line # 1 is the number of levels, L (a positive integer)
-    Line # 2 is the dimension of the grid, N (a positive integer)
-    Line # 3 is the number of initial lives, V (a positive integer)
-    Line # 4 is the approximate percentage of the positions in each level with coins (a positive integer)
-    Line # 5 is the approximate percentage of the positions in each level with nothing (a positive integer)
-    Line # 6 is the approximate percentage of the positions in each level with Goombas (a positive integer)
-    Line # 7 is the approximate percentage of the positions in each level with Koopas (a positive integer)
-    Line # 8 is the approximate percentage of the positions in each level with mushrooms (a positive integer)
-    
-    Values on line 4-8 MUST sum to 100.
-    */
     ifstream inFile(inputFile);
     if (!inFile.is_open()){
         cout << "Error opening input file!!" << endl;
@@ -31,6 +32,7 @@ void FileProcessor::readInputFile(const char* inputFile){
     // mark that the file is read successfully.
     _isSucceed = 1;
     
+    // read lines of the input file
     for (int i = 0; i < 8; i++){
         string line;
         getline(inFile, line);
